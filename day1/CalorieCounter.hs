e33e43af195b8f07a912883e61d7cb36176c6b45
Jeremy Sigrist <jsigrist1@gmail.com>
2022-12-21 22:35:19 -0800

    Finish day1

---
+++ day1/CalorieCounter.hs
@@ -0,0 +1,22 @@
+import qualified Data.Text as T
+import qualified Data.List as L
+import qualified Data.Maybe as M
+import System.IO
+import Text.Read
+import qualified Data.IntMap.Strict as Data.List
+
+topThree :: [Int] -> [Int]
+topThree ls = take 3 $ L.sortBy (flip compare) ls
+
+readInputData :: T.Text -> IO [Int]
+readInputData filename = do
+  handle <- openFile (T.unpack filename) ReadMode
+  contents <- hGetContents handle
+  let notListOfEmpty x = x /= [""]
+  let foodsPerElf = filter notListOfEmpty $ L.groupBy (\x y -> null x == null y) $ Prelude.lines contents
+  return $ map (sum . M.mapMaybe (readMaybe :: String -> Maybe Int)) foodsPerElf
+
+main :: IO ()
+main = do
+  x <- readInputData $ T.pack "./input.txt"
+  print $ sum $ topThree x
