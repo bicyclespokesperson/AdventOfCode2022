6cf9be5a4b37aa78391cbc55eb408e4a339ba627
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-02 16:26:30 -0800

    Finish day6
    Run formatter

--- day6/Message.hs
+++ day6/Message.hs
@@ -1,27 +1,28 @@
+import Data.Foldable (toList)
 import qualified Data.Sequence as S
 import qualified Data.Set as Set
-import Data.Foldable (toList)
 
-firstFour :: String -> (S.Seq Char, [Char])
-firstFour (a:b:c:d:xs) = (S.fromList [a, b, c, d], xs)
+firstN :: Int -> String -> (S.Seq Char, [Char])
+firstN n s = (S.fromList (take n s), drop n s)
 
 unique :: S.Seq Char -> Bool
-unique seq = let set = Set.fromList $ toList seq
-              in length seq == length set
+unique seq =
+  let set = Set.fromList $ toList seq
+   in length seq == length set
 
---f cur (x:xs) idx = let next = 
-f :: S.Seq Char -> [Char] -> Int -> Int
-f cur (hd:rs) idx 
-                | unique cur = idx
-                | otherwise = case S.viewl cur of 
-                    S.EmptyL -> 0
-                    x S.:< xs -> f (xs S.|> hd) rs (idx + 1)
-f _ [] _ = -1
-
+firstUniqueSubstr :: S.Seq Char -> String -> Int -> Int
+firstUniqueSubstr cur (hd:rs) idx
+  | unique cur = idx
+  | otherwise =
+    case S.viewl cur of
+      S.EmptyL -> -1
+      x S.:< xs -> firstUniqueSubstr (xs S.|> hd) rs (idx + 1)
+firstUniqueSubstr _ [] _ = -1
 
 main :: IO ()
 main = do
+  let n = 14
   contents <- readFile "input.txt"
-  let (start, rest) = firstFour contents
-  let idx = f start rest 4
+  let (start, rest) = firstN n contents
+  let idx = firstUniqueSubstr start rest n
   print idx
