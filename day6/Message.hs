394fdd71519c9da87894360d65610f7c04d1f6b6
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-02 16:20:21 -0800

    Solve day6 part 1

--- day6/Message.hs
+++ day6/Message.hs
@@ -1,14 +1,27 @@
 import qualified Data.Sequence as S
+import qualified Data.Set as Set
+import Data.Foldable (toList)
 
 firstFour :: String -> (S.Seq Char, [Char])
 firstFour (a:b:c:d:xs) = (S.fromList [a, b, c, d], xs)
 
 unique :: S.Seq Char -> Bool
-unique _ = True --TODO: implement
+unique seq = let set = Set.fromList $ toList seq
+              in length seq == length set
 
---f cur (x:xs) = let next = 
+--f cur (x:xs) idx = let next = 
+f :: S.Seq Char -> [Char] -> Int -> Int
+f cur (hd:rs) idx 
+                | unique cur = idx
+                | otherwise = case S.viewl cur of 
+                    S.EmptyL -> 0
+                    x S.:< xs -> f (xs S.|> hd) rs (idx + 1)
+f _ [] _ = -1
+
+
 main :: IO ()
 main = do
-  contents <- readFile "sample_input.txt"
+  contents <- readFile "input.txt"
   let (start, rest) = firstFour contents
-  putStrLn "test"
+  let idx = f start rest 4
+  print idx
