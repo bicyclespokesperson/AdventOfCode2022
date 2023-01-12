f2c4246ec96e2f230d62954f8027f74eed3a1ad2
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-11 19:40:48 -0800

    More work

--- day7/Directories.hs
+++ day7/Directories.hs
@@ -1,19 +1,28 @@
-import qualified Data.Text as T
-import qualified Data.Map as M
-import qualified Data.List as L
 import Control.Monad.State
+import qualified Data.Maybe as MB
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Text as T
 
---TODO: Update this to use Data.Text
+cdPrefix = T.pack "$ cd" :: T.Text
+lsPrefix = T.pack "$ ls" :: T.Text
 
-parseLine2 :: [String] -> M.Map [String] Integer -> State [String] (M.Map [String] Integer)
-parseLine2 ("$ cd ":dir) fileSizes = do
-                                curr <- get
-                                put (head dir : curr)
-                                return fileSizes
-
+parseLine ::
+     T.Text -> M.Map [T.Text] Integer -> State [T.Text] (M.Map [T.Text] Integer)
+parseLine cmd fileSizes
+  | T.isPrefixOf cdPrefix cmd = do
+    let dir = T.splitOn (T.pack ",") . MB.fromJust $ T.stripPrefix cdPrefix cmd
+    curr <- get
+    put (dir : curr)
+    return fileSizes
+  | T.isPrefixOf lsPrefix cmd = do
+    let dir = cmd
+    curr <- get
+    put (dir : curr)
+    return fileSizes
 
 {-
-parseLine3 :: [String] -> M.Map [String] Integer -> State [String] (M.Map [String] Integer)
+parseLine3 :: [T.Text] -> M.Map [T.Text] Integer -> State [T.Text] (M.Map [T.Text] Integer)
 parseLine3 cmd fileSizes = do
                                 curr <- get
                                 put cmd : curr
@@ -21,14 +30,13 @@ parseLine3 cmd fileSizes = do
 -}
 {-
 -- I need several functions that return a State. No functions will accept a state as a parameter
-parseLine :: [String] -> State (String, M.Map [String] Integer) ()
+parseLine :: [T.Text] -> State (T.Text, M.Map [T.Text] Integer) ()
 parseLine ("$":_) = do 
     let x = 5
     let y = M.fromList [([""], 3)]
     put ("test", y)
     return ()
     -}
-
 main :: IO ()
 main = do
-    print "test"
+  print "test"
