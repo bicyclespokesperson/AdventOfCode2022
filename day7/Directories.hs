a8bfb5a5d37419b02a610a983fba22a10d1f6e1f
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-11 20:36:14 -0800

    Making progress!

--- day7/Directories.hs
+++ day7/Directories.hs
@@ -4,15 +4,20 @@ import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Text.Read (readMaybe)
 
 cdHome = T.pack "$ cd /"
-cdPrefix = T.pack "$ cd"
+cdUp = T.pack "$ cd .."
+cdPrefix = T.pack "$ cd "
 lsPrefix = T.pack "$ ls"
 
 parseLine :: T.Text -> M.Map [T.Text] Integer -> State [T.Text] (M.Map [T.Text] Integer)
 parseLine cmd fileSizes
   | cdHome == cmd = do
     put []
+    return fileSizes
+  | cdUp == cmd = do
+    modify tail -- The state is a list of directories, with the first elem being the lowest level
     return fileSizes
   | T.isPrefixOf cdPrefix cmd = do
     let dir = reverse .T.splitOn (T.pack "/") . MB.fromJust $ T.stripPrefix cdPrefix cmd
@@ -23,10 +28,13 @@ parseLine cmd fileSizes
   | T.null cmd = do
     return fileSizes
   | otherwise = do
-    let (sz, name) = T.break (/= ' ') cmd
-    curr <- get
-    let fullPath = name : curr
-    return $ M.insert fullPath (read (T.unpack sz) :: Integer) fileSizes
+    let (sz, name) = T.span (/= ' ') cmd
+    case (readMaybe (T.unpack sz) :: Maybe Integer) of
+        (Just size) -> do
+            curr <- get
+            let fullPath = T.strip name : curr
+            return $ M.insert fullPath size fileSizes
+        Nothing -> return fileSizes -- Ignore directories for now
 
 main :: IO ()
 main = do
