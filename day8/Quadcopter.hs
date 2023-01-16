73ecc8b8024b482a3b078a8de5f7a540f8eff2b8
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-15 21:09:44 -0800

    Finish day 8

--- day8/Quadcopter.hs
+++ day8/Quadcopter.hs
@@ -25,23 +25,43 @@ left (i, j) = (i, j - 1)
 right :: (Int, Int) -> (Int, Int)
 right (i, j) = (i, j + 1)
 
-findEdge ::
+findEdge1 ::
      M.Map (Int, Int) Int -> ((Int, Int) -> (Int, Int)) -> (Int, Int) -> Bool
-findEdge mp dirFn coord =
+findEdge1 mp dirFn coord =
+  let height = mp M.! coord
+      loop cc =
+        case M.lookup cc mp of
+          (Just h) -> h < height && loop (dirFn cc)
+          Nothing -> True
+   in loop (dirFn coord)
+
+findEdge2 ::
+     M.Map (Int, Int) Int -> ((Int, Int) -> (Int, Int)) -> (Int, Int) -> Int
+findEdge2 mp dirFn coord =
   let height = mp M.! coord
       loop cc dist =
         case M.lookup cc mp of
-          (Just h) -> h < height && loop (dirFn cc) (dist + 1)
-          Nothing -> True
+          (Just h) -> if h < height then loop (dirFn cc) (dist + 1) else dist + 1
+          Nothing -> dist
    in loop (dirFn coord) 0
 
 part1 :: IO ()
 part1 = do
   grid <- readInputData "input.txt"
-  let findEdgeAllDirs = map (findEdge grid) [left, right, up, down]
+  let findEdgeAllDirs = map (findEdge1 grid) [left, right, up, down]
   let isVisible coord = any ($ coord) findEdgeAllDirs
   let res = M.foldrWithKey (\coord _ count -> count + if isVisible coord then 1 else 0) 0 grid
   print res
 
+part2 :: IO ()
+part2 = do
+  grid <- readInputData "input.txt"
+  print $ findEdge2 grid right (1, 2)
+  print $ findEdge2 grid down (1, 2)
+  let findEdgeAllDirs = map (findEdge2 grid) [left, right, up, down]
+  let scenicScore coord = product $ map ($ coord) findEdgeAllDirs
+  let res = M.foldrWithKey (\coord _ score -> max score (scenicScore coord)) (0 :: Int) grid
+  print res
+
 main :: IO ()
-main = part1
+main = part2
