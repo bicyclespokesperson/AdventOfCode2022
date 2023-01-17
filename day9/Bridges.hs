f9d35825b7913fcc733f08e79baf7a2036e9f333
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-16 22:00:33 -0800

    Printing input data

--- day9/Bridges.hs
+++ day9/Bridges.hs
@@ -1,6 +1,6 @@
 
 -- Left and Right are prelude functions, so disambiguate with *Move
-data Move = UpMove | DownMove | LeftMove | RightMove
+data Move = UpMove | DownMove | LeftMove | RightMove deriving Show
 
 toMove :: String -> Move
 toMove "U" = UpMove
@@ -13,12 +13,13 @@ parseLine s = let [dir, count] = words s
                in replicate (read count :: Int) (toMove dir)
 
 readInputData :: String -> IO [Move]
-readInputData filename = do
-                            contents <- lines <$> readFile filename
-                            return $ concatMap parseLine contents
+readInputData filename = concatMap parseLine . lines <$> readFile filename
 
 tailLocation :: (Int, Int) -> (Int, Int) -> Move -> (Int, Int)
 tailLocation headLoc tailLoc mv = (3, 4)
 
 main :: IO ()
-main = print "test"
+main = do
+          contents <- readInputData "sample_input.txt"
+          print contents
+          return ()
