ecf080d0ebfa0ecacc71d6393e52d27825b251b7
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-17 00:06:11 -0800

    Run formatter

--- day9/Bridges.hs
+++ day9/Bridges.hs
@@ -1,4 +1,5 @@
 import Data.Foldable (foldl')
+import Data.List (scanl')
 import qualified Data.Set as S
 
 -- Left and Right are prelude functions, so disambiguate with *Move
@@ -36,7 +37,14 @@ parseLine s =
 readInputData :: String -> IO [Move]
 readInputData filename = concatMap parseLine . lines <$> readFile filename
 
--- (x,y) is head location, (a,b) is tail location
+{-
+(x,y) is head location, (a,b) is tail location
+
+Another clever way of approaching this is to have a neighborhoodOf function that returns the 8 points around a point,
+then use logic based on the intersection (e.g. if tail is in heads neighbors, do nothing, otherwise choose the 
+point in the intersection of head and tail's neighbors that is closest to tail)
+-}
+
 tailLocation :: (Int, Int) -> (Int, Int) -> (Int, Int)
 tailLocation (x, y) (a, b)
   | abs (x - a) <= 1 && abs (y - b) <= 1 = (a, b)
@@ -50,16 +58,11 @@ tailLocation (x, y) (a, b)
   | x > a && y > b = (a + 1, b + 1)
 
 tailLocations :: [(Int, Int)] -> [(Int, Int)]
-tailLocations = scanl (flip tailLocation) (0, 0)
+tailLocations = scanl' (flip tailLocation) (0, 0)
 
 main :: IO ()
 main = do
   headMoves <- readInputData "input.txt"
-  let headLocations =
-        reverse $
-        foldl'
-          (\coords mv -> makeMove (head coords) mv : coords)
-          [(0, 0)]
-          headMoves
+  let headLocations = scanl' makeMove (0, 0) headMoves
   let allTailLocations = iterate tailLocations headLocations !! 9 -- Set this to 1 for part 1
   print . S.size $ S.fromList allTailLocations
