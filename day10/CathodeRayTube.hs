25ab2ae2a7edc2ec746ab5baf2e82dd302955f51
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-18 18:50:15 -0800

    Solve day10 part 1

--- day10/CathodeRayTube.hs
+++ day10/CathodeRayTube.hs
@@ -1,2 +1,17 @@
+
+parseLine :: String -> [Int -> Int]
+parseLine "noop" = [id]
+parseLine addX = [id, (+) (read x :: Int)] where
+    [_, x] = words addX
+
+readInputData :: String -> IO [Int -> Int]
+readInputData filename = concatMap parseLine . lines <$> readFile filename
+
+analyzeResults :: [Int] -> Int
+analyzeResults xs = sum . map (uncurry (*)) $ filter (\(i, x) -> (i - 20) `mod` 40 == 0) $ zip [1..] xs
+
 main :: IO ()
-main = print "test"
+main = do
+        fs <- readInputData "sample_input_2.txt"
+        let res = scanl (\v f -> f v) 1 fs
+        print $ analyzeResults res
