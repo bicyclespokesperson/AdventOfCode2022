4e21e37eb2291d7ee73d48131a8b1cf865cdfa6f
Jeremy Sigrist <jsigrist1@gmail.com>
2023-01-31 21:04:44 -0800

    Finish day11 part 1

--- day11/bin/KeepAway.hs
+++ day11/bin/KeepAway.hs
@@ -7,9 +7,11 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer (decimal)
 import Data.Void (Void)
+import Data.List (sort)
 import qualified Data.Sequence as S
 import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Functor (($>))
+import Control.Lens ((.~), element)
 
 type Parser = Parsec Void String
 
@@ -18,7 +20,8 @@ data Monkey = Monkey {
     operation :: Int -> Int,
     test :: Int -> Bool,
     trueTarget :: Int,
-    falseTarget :: Int
+    falseTarget :: Int,
+    totalSeen :: Int
 };
 
 -- parse a list of integers separated by commas
@@ -60,19 +63,50 @@ parseMonkey = do
   _ <- space
   falseTarget <- string "If false: throw to monkey " *> L.decimal
   _ <- space
+  let totalSeen = 0 -- length items
   return Monkey {..}
 
 pushItem :: Int -> Monkey -> Monkey
 pushItem val m = let newItems = (items m S.|> val) in m {items = newItems} 
 
 popItem :: Monkey -> (Monkey, Int)
-popItem m = case S.viewr (items m) of
-              S.EmptyR -> (m, -1) -- TODO: Should this be an error?
-              (rest S.:> end) -> (m {items = rest}, end)
+popItem m = case S.viewl (items m) of
+              S.EmptyL -> error "popItem: Empty sequence"
+              (start S.:< rest) -> (m {items = rest, totalSeen = totalSeen m + 1}, start)
+
+switchItem :: Int -> Int -> Int -> [Monkey] -> [Monkey]
+switchItem from to new ms = let (m, _) = popItem $ ms !! from 
+                         in (element from .~ m) $ (element to .~ pushItem new (ms !! to)) ms
+
+boredomAdjustment :: Int -> Int
+boredomAdjustment itm = itm `div` 3
+
+inspectItem :: Int -> [Monkey] -> [Monkey]
+inspectItem i ms = let monkey = (ms !! i)
+                       itm = S.index (items monkey) 0
+                       moreWorried = operation monkey itm
+                       lessWorried = boredomAdjustment moreWorried
+                       target = (if test monkey lessWorried then trueTarget else falseTarget) monkey
+                    in switchItem i target lessWorried ms
+
+runRound' :: Int -> [Monkey] -> [Monkey]
+runRound' i ms
+  | i >= length ms = ms
+  | null (items $ ms !! i) = runRound' (i + 1) ms
+  | otherwise = runRound' i $ inspectItem i ms
+
+runRound :: [Monkey] -> [Monkey]
+runRound = runRound' 0
+
+calculateViews :: [Monkey] -> [Int]
+calculateViews ms = let states = take 21 $ iterate runRound ms
+                     in map totalSeen $ last states
 
 main :: IO ()
 main = do
-  contents <- readFile "sample_input.txt"
+  contents <- readFile "input.txt"
   case runParser (many parseMonkey) "filename_for_error_message.txt" contents of
       Left e -> putStr (errorBundlePretty e)
-      Right r -> print (items (r !! 2))
+      Right monkeys -> do print $ map items monkeys
+                          print . map items $ runRound monkeys
+                          print $ product . take 2 . reverse . sort $ calculateViews monkeys
