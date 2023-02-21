19ff24115432bac7153cfedec5381f99f9218745
Jeremy Sigrist <jsigrist1@gmail.com>
2023-02-20 20:57:12 -0800

    Remove unused code

--- day12/bin/HillClimb.hs
+++ day12/bin/HillClimb.hs
@@ -6,11 +6,8 @@ import Data.Char (ord)
 import qualified Data.Array as A
 import Data.Void (Void)
 import qualified Data.Set as S
-import qualified Control.Monad.State as MS
-import Control.Monad (foldM, guard, (<=<))
+import Control.Monad (guard)
 import Data.Maybe (fromJust)
-import Data.List (foldl1')
-import qualified Control.Monad.Loops as CML
 
 type Coord2 = (Int, Int)
 type Grid2D a = A.Array Coord2 a
@@ -35,62 +32,19 @@ neighbors :: Grid2D Int -> Coord2 -> [Coord2]
 neighbors grid (x, y) = do 
                             let ((xmin, ymin), (xmax, ymax)) = A.bounds grid
                             (x', y') <- [(x+1, y), (x-1,y), (x, y+1), (x, y-1), (x, y)]
-                            guard (x' `elem` [xmin..xmax] && y' `elem` [ymin..ymax] && ((grid A.! (x', y')) - (grid A.! (x, y))) <= 1)
+                            _ <- guard (x' `elem` [xmin..xmax] && y' `elem` [ymin..ymax] && ((grid A.! (x', y')) - (grid A.! (x, y))) <= 1)
                             return (x', y')
 
 singleStep :: Grid2D Int -> S.Set Coord2 -> S.Set Coord2
 singleStep grid =  let neighbors' = neighbors grid
                     in S.foldl' (\a b -> S.union a (S.fromList $ neighbors' b)) S.empty
 
--- Potential problem: If a square is reachable from two current squares, it will be 
--- present twice in the resulting list. I could, potentially, create a new function 
--- visit all neighbors, updating the state at each iteration. That would defeat the 
--- entire purpose of using the list monad though.
--- Also, we only want, really, the new visited nodes present at the end of this?
-{-
-neighbors' :: Coord2 -> Coord2 -> Grid2D Int -> Coord2 -> MS.State (S.Set Coord2) [Coord2]
-neighbors' (xmin, ymin) (xmax, ymax) grid (x, y) = do
-    visited <- MS.get
-    let newNeighbors' = [(x+1, y), (x-1,y), (x, y+1), (x, y-1)] >>= (\(x', y') -> guard (x' `elem` [xmin..xmax] 
-                                                                                         && y' `elem` [ymin..ymax] 
-                                                                                         && (((grid A.! (x', y')) - (grid A.! (x, y))) <= 1) 
-                                                                                         && (x', y') `S.notMember` visited) >> return (x', y'))
-    MS.put (S.union visited (S.fromList newNeighbors'))
-    return newNeighbors'
--}
-
-
-{-
-fewestSteps :: Grid2D Int -> Coord2 -> Coord2 -> Int
-fewestSteps grid start end = let (min', max') = A.bounds grid
-                                 f = neighbors min' max' grid
-                                 t = iterate (>>= f) [start]
-                              in length $ takeWhile (notElem end) t
-
-fewestSteps' :: Grid2D Int -> Coord2 -> Coord2 -> [[Coord2]]
-fewestSteps' grid start end = let (min', max') = A.bounds grid
-                                  f = neighbors' min' max' grid
-                                  stepFn :: [Coord2] -> MS.State (S.Set Coord2) [Coord2]
-                                  stepFn cur = let newNeighbors = mapM f cur in concat <$> newNeighbors
-                                  states = CML.iterateUntilM (\xs -> True) stepFn [start]
-                                  (curr, _) = MS.runState states S.empty
-                                in [curr]
-                                -}
-
 find :: Grid2D Int -> Int -> Maybe Coord2
 find grid val = case filter (\(_, e) -> val == e) (A.assocs grid) of
                   ((i, _):_) -> Just i
                   [] -> Nothing
 
-printHelper :: [[Coord2]] -> IO [()]
-printHelper = traverse print
-
--- https://stackoverflow.com/questions/25780555/best-way-to-generate-a-list-with-state-haskell
--- https://cloudnative.ly/haskell-mapping-with-state-7a07e3c2cbf9
-
---x = sequenceA
-
-fewestSteps :: Grid2D Int -> Coord2 -> Coord2 -> Int --[S.Set Coord2]
+fewestSteps :: Grid2D Int -> Coord2 -> Coord2 -> Int
 fewestSteps grid start end = let g = singleStep grid
                                  vals = iterate g $ S.fromList [start]
                               --in vals
@@ -98,7 +52,7 @@ fewestSteps grid start end = let g = singleStep grid
 
 part1 :: IO ()
 part1 = do
-  contents <- readFile "sample_input.txt"
+  contents <- readFile "input.txt"
   case runParser parse2DDigitArray "filename_for_error_message.txt" contents of
       Left e -> putStr (errorBundlePretty e)
       Right g -> do
