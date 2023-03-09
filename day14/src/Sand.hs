8264767b76a800a04d4d34410f209f66def80549
Jeremy Sigrist <jsigrist1@gmail.com>
2023-03-08 18:45:54 -0800

    Start on day 14

---
+++ day14/src/Sand.hs
@@ -0,0 +1,22 @@
+module Main where
+
+--See https://github.com/clatisus/advent-of-code-y2022/blob/master/src/Day13.hs for inspiration
+
+import Data.Maybe (fromJust)
+import Data.List (sort, elemIndex)
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as LL (decimal)
+
+
+part1 :: IO ()
+part1 = do
+  print "test"
+
+part2 :: IO ()
+part2 = do
+  print "test"
+
+main :: IO ()
+main = part1
