3a24f73f7faf232686040927cf0bc046ee05cd22
Jeremy Sigrist <jsigrist1@gmail.com>
2023-03-11 22:31:38 -0800

    Finish day14

--- day14/src/Sand.hs
+++ day14/src/Sand.hs
@@ -12,6 +12,9 @@ type Parser = Parsec Void String
 
 type Cave = S.Set (Int, Int)
 
+entryPoint :: (Int, Int)
+entryPoint = (500, 0)
+
 readCoordinatePair :: Parser (Int, Int)
 readCoordinatePair = do
                     x <- LL.decimal
@@ -38,6 +41,14 @@ dropSand cave maxY (x, y)
   | not (S.member (x+1, y+1) cave) = dropSand cave maxY (x+1, y+1)
   | otherwise = (S.insert (x, y) cave, True)
 
+dropSand' :: Cave -> Int -> (Int, Int) -> (Cave, Bool)
+dropSand' cave maxY (x, y)
+  | y >= maxY = (S.insert (x, y) cave, True)
+  | not (S.member (x, y+1) cave) = dropSand' cave maxY (x, y+1)
+  | not (S.member (x-1, y+1) cave) = dropSand' cave maxY (x-1, y+1)
+  | not (S.member (x+1, y+1) cave) = dropSand' cave maxY (x+1, y+1)
+  | otherwise = (S.insert (x, y) cave, (x, y) /= entryPoint)
+
 part1 :: IO ()
 part1 = do
   contents <- readFile "input.txt"
@@ -50,14 +61,25 @@ part1 = do
                       let f cave' startPos = let (cave'', landed) = dropSand cave' maxY startPos 
                                              in if landed then f cave'' startPos else cave'
                       
-                      let finalCave = f cave (500, 0)
+                      let finalCave = f cave entryPoint
 
                       print $ S.size finalCave - S.size cave
 
 part2 :: IO ()
 part2 = do
-  contents <- readFile "sample_input.txt"
-  print $ head contents
+  contents <- readFile "input.txt"
+  case runParser readRocks "filename_for_error_message.txt" contents of
+    Left err -> putStr (errorBundlePretty err)
+    Right rocks -> do
+                      let cave = foldl addRock S.empty rocks
+                      let maxY = S.foldl' (\a b -> max a (snd b)) 0 cave + 1
+
+                      let f cave' startPos = let (cave'', landed) = dropSand' cave' maxY startPos 
+                                             in if landed then f cave'' startPos else cave'
+                      
+                      let finalCave = f cave entryPoint
+
+                      print $ S.size finalCave - S.size cave + 1
 
 main :: IO ()
-main = part1
+main = part2
